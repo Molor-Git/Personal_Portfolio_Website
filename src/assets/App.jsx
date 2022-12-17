5ce924a9b2eaccb1621b3f464235889a3ea577aa
Molor <mongolhuu0731@gmail.com>
2022-12-17 12:29:04 -0500

    first commit

---
+++ src/assets/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+// import Header from './components'
+const App = () => 
+{
+	return <div>App</div>;
+};
+
+export default App;
---
+++ src/components/services/Servises.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import './services.css'
+
+const Servises = () => {
+	return <div>Servises</div>;
+};
+
+export default Servises;
